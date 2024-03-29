0471b78973cc61a79d28b434221d7dbde0b973d0
Nirob_Barman <niroborin111@gmail.com>
2024-03-29 09:50:22 +0600

    Express & MongoDB Rest API

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal,
+} = require('../controllers/goalController.js')
+
+// app.get('/api/goals', (req, res) => {
+//     res.status(200).send({ message: 'Get goals' })
+// })
+
+// router.get('/', (req, res) => {
+//     res.status(200).send({ message: 'Get goals' })
+// })
+
+// router.post('/', (req, res) => {
+//     res.status(200).json({ message: 'Set goal' })
+// })
+
+// router.put('/:id', (req, res) => {
+//     res.status(200).json({ message: `Update goal ${req.params.id}` })
+// })
+
+// router.delete('/:id', (req, res) => {
+//     res.status(200).json({ message: `Delete goal ${req.params.id}` })
+// })
+
+router.route('/').get(getGoals).post(setGoal)
+router.route('/:id').delete(deleteGoal).put(updateGoal)
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xceqs5c.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
+
+// app.get('/api/goals', (req, res) => {
+//     res.status(200).send({ message: 'Get goals' })
+// })
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
